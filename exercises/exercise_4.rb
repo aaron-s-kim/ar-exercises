1b96094b6c768ea65ed0902102fc9480d4cf531b
Aaron K <tonberryspawn@gmail.com>
2022-01-03 20:18:13 -0800

    exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -9,14 +9,19 @@ puts "----------"
 # Your code goes here ...
 
 # Loading a subset of stores
-store = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
-store = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
-store = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true).order(created_at: :desc)
 
-puts @mens_stores
-
 @mens_stores.each do |s|
+  puts "#{s.name} #{s.annual_revenue}"
+end
+
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: (0..1000000))
+
+@womens_stores.each do |s|
   puts "#{s.name} #{s.annual_revenue}"
 end
