1c2cafc3fb6e734b289fc24c859cd5e694b231b3
Aaron K <tonberryspawn@gmail.com>
2022-01-03 19:59:16 -0800

    ex 1-4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Update the first store
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = 'Vancouver'
+@store1.save
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# Loading a subset of stores
+store = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true).order(created_at: :desc)
+
+puts @mens_stores
+
+@mens_stores.each do |s|
+  puts "#{s.name} #{s.annual_revenue}"
+end
