a9f7f4f642fa3b77ff2ba7961cc402d380dfbb51
Aaron K <tonberryspawn@gmail.com>
2022-01-03 20:51:50 -0800

    exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -9,3 +9,8 @@ puts "----------"
 
 # Your code goes here ...
 
+# Calculations
+puts Store.sum("annual_revenue")
+puts Store.average("annual_revenue")
+
+puts Store.where('annual_revenue > 1000000').count # => 2
